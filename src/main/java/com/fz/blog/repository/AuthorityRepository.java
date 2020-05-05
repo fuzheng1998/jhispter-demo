fb2d78041e44cc2fe92c025898f3cfa992a91a11
付峥 <fuzheng1998@gmail.com>
2020-05-05 08:54:41 +0800

    Initial version of jhipster generated by JHipster-6.8.0

---
+++ src/main/java/com/fz/blog/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.fz.blog.repository;
+
+import com.fz.blog.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/fz/blog/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.fz.blog.web.rest;
